20b4bbe3be7bbf42202e24f652f2f7604e6a6915
ushanil <ush.gopi@gmail.com>
2019-11-06 11:16:42 -0500

    BlackJackPlayer.java CrapsPlayer.java GoFishPlayer.java KlondikePlayer.java

--- src/main/java/models/people/players/CrapsPlayer.java
+++ src/main/java/models/people/players/CrapsPlayer.java
@@ -2,16 +2,25 @@ package models.people.players;
 
 import Interfaces.GamblingPlayer;
 import models.gamecomponents.Dice;
+import services.PlayerService;
 
 public class CrapsPlayer implements GamblingPlayer {
     Player crapsPlayer;
+    final Double betAmount;
+    PlayerService ps;
+
+
 
     public CrapsPlayer(Player player) {
+
         this.crapsPlayer = player;
+        betAmount = 1.0;
+        ps = new PlayerService(crapsPlayer);
     }
 
     public Double placeBet() {
-        return null;
+        Double balanceAfterPlacingBet = ps.depositMoney( betAmount);
+        return balanceAfterPlacingBet;
     }
 
     public Integer rollDice(Dice dice){
--- src/main/java/models/people/players/GoFishPlayer.java
+++ src/main/java/models/people/players/GoFishPlayer.java
@@ -2,13 +2,19 @@ package models.people.players;
 
 import models.gamecomponents.Card;
 import models.hands.GoFishHand;
+import services.PlayerService;
 
 public class GoFishPlayer extends Player {
     Player goFishPlayer;
     GoFishHand hand = new GoFishHand();
+    final Double betAmount;
+    PlayerService ps;
 
     public GoFishPlayer(Player player) {
+
         this.goFishPlayer = player;
+        betAmount = 1.0;
+        ps = new PlayerService(goFishPlayer);
     }
 
     public Integer askForCard(){
